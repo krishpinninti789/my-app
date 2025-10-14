9708ccba1a6470e3696fa841585dcb7e964ea7da
Krishnavamsi P <krishnavamsi.p@MB-M-KRISHNAVMSI.local>
2025-10-14 12:01:58 +0530

    type definitions

---
+++ src/app/utils/userData.ts
@@ -0,0 +1,29 @@
+export const users:users[] = [
+    {
+        id:1,
+        name:"krishna",
+        age:22
+    }
+    ,
+    {
+        id:2,
+        name:"vamsi",
+        age:19
+    },
+    {
+        id:3,
+        name:"naidu",
+        age:21
+    },
+    {
+        id:4,
+        name:"deepak",
+        age:23
+    }
+]
+
+interface users{
+    id:Number,
+    name:String,
+    age:number
+}
